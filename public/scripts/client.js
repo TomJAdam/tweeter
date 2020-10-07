2e8bf1161dabe95fee435087711782b7193dc289
Tom Adam <tom.adam21@gmail.com>
2020-10-07 01:03:38 +0000

    client.js tweet GET route functioning

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = (tweets) => {
   for (const tweet of tweets) {
     $("#tweets-container").append(createTweetElement(tweet))
@@ -55,11 +30,19 @@ return $tweet;
 
 $(document).ready(function() {
 
-  renderTweets(data);
-
   $(".new-tweet").find("form").submit(function(event) {
     event.preventDefault();
+    let serializedTweet = $(this).serialize();
+    $.post('/tweets', serializedTweet);
   });
-  
+
+  const loadTweets = () => {
+    $.get('/tweets', function(data) {
+      renderTweets(data)
+    }, "json");
+  };
+
+  loadTweets();
+
 });
 
